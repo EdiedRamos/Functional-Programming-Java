1660ca26f2a9c885af6295a0ca1e94f3b866a0ba
EdiedRamos <edieisa22@gmail.com>
2024-05-14 23:16:40 -0500

    chore: supplier and consumer

---
+++ src/main/java/org/example/ConsumerAndSupplier.java
@@ -0,0 +1,40 @@
+package org.example;
+
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class ConsumerAndSupplier {
+    static
+
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class CLIArguments {
+    private boolean isHelp;
+
+    public boolean isHelp() {
+        return isHelp;
+    }
+}
+
+class CLIArgumentsUtils {
+    static void showHelp(CLIArguments cliArguments) {
+        Consumer<CLIArguments> consumerHelper = cliArguments1 -> {
+            if (cliArguments1.isHelp()) {
+                System.out.println("Manual required!");
+            }
+        };
+        consumerHelper.accept(cliArguments);
+    }
+
+    static CLIArguments generateCLI() {
+//        Supplier<CLIArguments> generator = () -> new CLIArguments();
+//        With reference method
+        Supplier<CLIArguments> generator = CLIArguments::new;
+        return generator.get();
+    }
+}
